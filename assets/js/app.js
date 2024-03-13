ae37ae91e971bc1549894218580075611b1e91e7
danielecolaci <danielecolaci@outlook.com>
2024-03-13 19:30:00 +0100

    Add Functions on click the check span to move object on the other array. Fix Style

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,31 @@ createApp({
     data() {
         return {
             newTask: '',
-            taskList: [
-                { text: 'First Task', done: false },
-            ],
-            completed: [
-                { text: 'First Task Done', done: true },
-            ],
+            taskList: [],
+            completed: [],
         }
     },
     methods: {
         addTask() {
-            this.taskList.push({ text: this.newTask, done: false });
+            this.taskList.unshift({ text: this.newTask, done: false });
             this.newTask = '';
+        },
+        taskDone(index) {
+            const completedTask = this.taskList.splice(index, 1)[0];
+            completedTask.done = true;
+            this.completed.unshift(completedTask);
+        },
+        taskNotDone(index) {
+            const uncompletedTask = this.completed.splice(index, 1)[0];
+            uncompletedTask.done = false;
+            this.taskList.unshift(uncompletedTask);
         }
     },
 
     mounted() {
+
+
+
 
     },
 
