430bd699fb748a63c991a7abb332ebe35789b049
danielecolaci <danielecolaci@outlook.com>
2024-03-13 17:11:45 +0100

    Add dynamics task from js arrays, fix style and code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
-console.log('ok');
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            newTask: '',
+            taskList: [
+                { text: 'First Task', done: false },
+                { text: 'Second Task', done: false },
+            ],
+            completed: [
+                { text: 'First Task Done', done: true },
+            ],
+        }
+    },
+    methods: {
+
+    },
+
+    mounted() {
+
+    },
+
+
+}).mount('#app')
